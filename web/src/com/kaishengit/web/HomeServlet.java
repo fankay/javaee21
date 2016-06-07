26961ef744537f0c63a29970819a36741a73d924
fankay <fankai@kaishengit.com>
2016-06-07 15:11:28 +0800

    演示Servlet3.0中Servlet的注解使用

---
+++ web/src/com/kaishengit/web/HomeServlet.java
@@ -0,0 +1,17 @@
+package com.kaishengit.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/main")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Hello,Servlet!");
+    }
+}
