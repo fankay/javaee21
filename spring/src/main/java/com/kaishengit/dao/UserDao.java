e574ba805b63523749b9111500c08f94cc31b163
fankay <fankai@kaishengit.com>
2016-07-01 10:23:54 +0800

    演示Spring中jdbc的使用

--- spring/src/main/java/com/kaishengit/dao/UserDao.java
+++ spring/src/main/java/com/kaishengit/dao/UserDao.java
@@ -1,7 +1,17 @@
 package com.kaishengit.dao;
 
+import com.kaishengit.pojo.User;
+
+import java.util.List;
+
 public interface UserDao {
 
-    Integer save();
+    void save(User user);
+    void del(Integer id);
+    void update(User user);
+    User findById(Integer id);
+    List<User> findAll();
+    User findByUserName(String username);
+    Long count();
 
 }
--- spring/src/main/java/com/kaishengit/dao/UserDaoImpl.java
+++ spring/src/main/java/com/kaishengit/dao/UserDaoImpl.java
@@ -1,16 +1,72 @@
 package com.kaishengit.dao;
 
-import javax.inject.Named;
+import com.kaishengit.pojo.User;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.SingleColumnRowMapper;
 
-@Named("userDao")
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Named
 public class UserDaoImpl implements UserDao {
 
+    @Inject
+    private JdbcTemplate jdbcTemplate;
+
     @Override
-    public Integer save() {
-        System.out.println("userDao save...");
-        if(1==1) {
-            //throw new RuntimeException("出大事了....");
-        }
-        return 10000;
+    public void save(User user) {
+        String sql = "insert into t_user(username,password,address) values(?,?,?)";
+        jdbcTemplate.update(sql,user.getUsername(),user.getPassword(),user.getAddress());
+    }
+
+    @Override
+    public void del(Integer id) {
+        String sql = "delete from t_user where id = ?";
+        jdbcTemplate.update(sql,id);
+    }
+
+    @Override
+    public void update(User user) {
+        String sql = "update t_user set username = ?,password = ?,address = ? where id = ?";
+        jdbcTemplate.update(sql,user.getUsername(),user.getPassword(),user.getAddress(),user.getId());
+    }
+
+    @Override
+    public User findById(Integer id) {
+        String sql = "select * from t_user where id = ?";
+        return jdbcTemplate.queryForObject(sql, new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet resultSet, int i) throws SQLException {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("username"));
+                user.setPassword(resultSet.getString("password"));
+                user.setAddress(resultSet.getString("address"));
+                return user;
+            }
+        },id);
+    }
+
+    @Override
+    public List<User> findAll() {
+        String sql = "select * from t_user";
+        return jdbcTemplate.query(sql,new BeanPropertyRowMapper<>(User.class));
+    }
+
+    @Override
+    public User findByUserName(String username) {
+        String sql = "select * from t_user where username = ?";
+        return jdbcTemplate.queryForObject(sql,new BeanPropertyRowMapper<>(User.class),username);
+    }
+
+    @Override
+    public Long count() {
+        String sql = "select count(*) from t_user";
+        return jdbcTemplate.queryForObject(sql,new SingleColumnRowMapper<Long>());
     }
 }
